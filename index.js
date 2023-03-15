aae297578281b39b760e33fb9ab09b1ea6f0f58d
Deeo Raiser <lseDeeo Raiser@DEEO-PC>
2023-03-14 21:35:24 -0300

    Express funcionando y ruta GET definida

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send({
+    msg:`Bienvenido a mi servidor express`,
+    ok: true
+})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
